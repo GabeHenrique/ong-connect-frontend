9c5799e2265a37f6645be9e2e394809ff8462bd5
Gabriel Palermo <pallermmo2011@gmail.com>
2025-04-20 17:20:09 -0300

    corrigindo script

--- src/pages/api/auth/providers.ts
+++ src/pages/api/auth/providers.ts
@@ -1,8 +1,3 @@
 export default function handler(req, res) {
-  if (req.method === "GET") {
-    res.status(403).json({ error: "Access to providers is restricted." });
-  } else {
-    res.setHeader("Allow", ["GET"]);
-    res.status(405).end(`Method ${req.method} Not Allowed`);
-  }
+    res.status(200).json({ message: "ok" });
 }
